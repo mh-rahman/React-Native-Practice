4f551fc4b86edab547bf375fd7a2751c106c8635
mh-rahman <rahmanhaseeb32@gmail.com>
2020-08-07 06:52:58 -0700

    Using images

--- 1 Getting Started/rn-starter/src/components/ImageDetail.js
+++ 1 Getting Started/rn-starter/src/components/ImageDetail.js
@@ -1,8 +1,15 @@
 import React from "react";
-import { view, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 
 const ImageDetail = (props) => {
-  return <Text>This is image detail {props.title}.</Text>;
+  return (
+    <View>
+      <Text>
+        This is {props.title}. It's score is {props.imageScore}
+      </Text>
+      <Image source={props.imageSource} />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({});
--- 1 Getting Started/rn-starter/src/screens/ImageScreen.js
+++ 1 Getting Started/rn-starter/src/screens/ImageScreen.js
@@ -5,10 +5,21 @@ import ImageDetail from "../components/ImageDetail";
 const ImageScreen = () => {
   return (
     <View>
-      <ImageDetail title="Image 1" />
-      <ImageDetail title="Image 2" />
-      <ImageDetail title="Image 3" />
-      <ImageDetail title="Image 4" />
+      <ImageDetail
+        title="Beach"
+        imageSource={require("../../assets/beach.jpg")}
+        imageScore={10}
+      />
+      <ImageDetail
+        title="Forest"
+        imageSource={require("../../assets/forest.jpg")}
+        imageScore={7}
+      />
+      <ImageDetail
+        title="Mountain"
+        imageSource={require("../../assets/mountain.jpg")}
+        imageScore={8}
+      />
     </View>
   );
 };
